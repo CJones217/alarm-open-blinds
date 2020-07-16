192a72d95c30a5095b3250ac909156124dc95277
Collin Jones <cjones28@luc.edu>
2020-07-15 19:48:10 -0500

    added stepper motor logic, need to change speed and amount of rotations

--- alarm_open_blinds.ino
+++ alarm_open_blinds.ino
@@ -1,10 +1,16 @@
+
+#include <Stepper.h>
+const int stepsPerRevolution = 1500;
 int  sensorPin  =  A0;     // select the input  pin for  the potentiometer 
 int  ledPin  =  13;   // select the pin for  the LED
 int  sensorValue =  0;  // variable to  store  the value  coming  from  the sensor
 
+Stepper myStepper(stepsPerRevolution, 8, 10, 9, 11);
+
 void setup()
 {
 pinMode(ledPin,OUTPUT);
+myStepper.setSpeed(20);
 Serial.begin(9600);
 }
 void loop(){
@@ -13,8 +19,13 @@ sensorValue =  analogRead(sensorPin);
 if(sensorValue>225) {
   Serial.println(sensorValue,  DEC);
   digitalWrite(ledPin,  HIGH); 
-  delay(sensorValue); 
+  for(int i=0;i<5;i++){
+    Serial.println("clockwise");
+    myStepper.step(stepsPerRevolution);
+    delay(250);
   }
+  delay(sensorValue); 
+ }
 
 digitalWrite(ledPin,  LOW);
 delay(sensorValue);
